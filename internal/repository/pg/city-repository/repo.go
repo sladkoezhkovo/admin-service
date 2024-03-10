c82ac2fcdbb54779657c93d81c39fdd5a32d0c91
rE <haxrelt@gmail.com>
2024-03-10 18:00:32 +0300

    feature / cities' CRUD

--- internal/repository/pg/city-repository/repo.go
+++ internal/repository/pg/city-repository/repo.go
@@ -19,33 +19,44 @@ func New(db *sqlx.DB) *cityRepository {
 }
 
 func (c *cityRepository) Create(city *entity.City) error {
-	return c.db.QueryRowx(
-		"INSERT INTO city(name) VALUES ($1) RETURNING id",
-		city.Name,
-	).Scan(&city.Id)
+	return c.db.Get(city, "INSERT INTO city (name) VALUES ($1) RETURNING *", city.Name)
 }
 
 func (c *cityRepository) FindById(id int64) (*entity.City, error) {
-	//TODO implement me
-	panic("implement me")
+	var city entity.City
+	if err := c.db.Get(&city, "SELECT * FROM city WHERE id = $1", id); err != nil {
+		return nil, err
+	}
+	return &city, nil
 }
 
 func (c *cityRepository) FindByName(name string) (*entity.City, error) {
-	//TODO implement me
-	panic("implement me")
+	var city entity.City
+	if err := c.db.Get(&city, "SELECT * FROM city WHERE name = $1", name); err != nil {
+		return nil, err
+	}
+	return &city, nil
 }
 
 func (c *cityRepository) List(limit, offset int) ([]*entity.City, error) {
-	//TODO implement me
-	panic("implement me")
+	var cities []*entity.City
+	if err := c.db.Select(&cities, "SELECT * FROM city ORDER BY id LIMIT $1 OFFSET $2", limit, offset); err != nil {
+		return nil, err
+	}
+	return cities, nil
 }
 
 func (c *cityRepository) Update(city *entity.City) error {
-	//TODO implement me
-	panic("implement me")
+	return c.db.Get(city,
+		`UPDATE city
+				SET
+					name = $1
+				WHERE id = $2 RETURNING *`, city.Name, city.Id)
 }
 
 func (c *cityRepository) Delete(id int64) error {
-	//TODO implement me
-	panic("implement me")
+	if _, err := c.db.Exec("DELETE FROM city WHERE id = $1", id); err != nil {
+		return err
+	}
+	return nil
 }
