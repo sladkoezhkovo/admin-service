b0a4c35a4c778258ee2ca7cf0afceb568a5754f5
rE <haxrelt@gmail.com>
2024-03-10 14:29:12 +0300

    feature / update proto & create entities

---
+++ internal/entity/entities.go
@@ -0,0 +1,27 @@
+package entity
+
+type City struct {
+	Id   int64  `db:"id"`
+	Name string `db:"name"`
+}
+type PropertyType struct {
+	Id   int64  `db:"id"`
+	Name string `db:"name"`
+}
+type ConfectionaryType struct {
+	Id   int64  `db:"id"`
+	Name string `db:"name"`
+}
+type Unit struct {
+	Id   int64  `db:"id"`
+	Name string `db:"name"`
+}
+type Packaging struct {
+	Id   int64  `db:"id"`
+	Name string `db:"name"`
+}
+type District struct {
+	Id     int64  `db:"id"`
+	Name   string `db:"name"`
+	CityId int64  `db:"city_id"`
+}
